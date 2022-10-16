cff1f44cb611d61eb33a18a1a9f9f6e24fe53fc5
jsham042 <103616381+jsham042@users.noreply.github.com>
2022-10-16 09:12:34 -0700

    Made a lot of changes to the new components

--- src/components/Recommendation/Recommendation.js
+++ src/components/Recommendation/Recommendation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import DanceAbility from '../Danceability/Danceability';
 
 import './Recommendation.css';
 
@@ -7,22 +8,36 @@ class Recommendation extends React.Component {
     super(props);
 
     this.state = {
-      term: ''
+      genre: 'pop',
     };
 
     this.makeRecommendation = this.makeRecommendation.bind(this);
+    this.handleRecommendation = this.handleRecommendation.bind(this);
   }
   
-
   makeRecommendation() {
-    this.props.onRecommendation(this.state.term);
+    this.props.onRecommendation(this.state.genre);
+  }
+
+  handleRecommendation(event)
+  {
+    this.setState({genre: event.target.value})
   }
 
   render() {
     return (
       <div className="Recommendation">
-        <button className="Recommendation Button" onClick={this.makeRecommendation}>GET RECOMMENDATIONS</button>
-       </div>
+        <label for="exampleDataList" class="form-label">Enter Genre</label>
+        <input class="form-control" list="datalistOptions" id="exampleDataList" placeholder="Type to search...">
+        <datalist id="datalistOptions">
+            <option value="pop">
+            <option value="r-n-b">
+            <option value="reggaeton">
+            <option value="edm">
+            <option value="rock">
+          </datalist>
+          <button className="Recommendation Button" onClick={this.makeRecommendation}>GET RECOMMENDATIONS</button>
+        </div>
     );
   }
 }
